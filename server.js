aa07948d78e64b46fcf48021f20724e9ab8a8ad7
PavelPoroskov <pavel.poroskov.se@gmail.com>
2024-02-05 01:35:22 +0300

    login endpoints

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from 'express'
 import { JSONFilePreset } from 'lowdb/node'
 import * as url from 'url';
 import bcrypt from 'bcryptjs';
-import * as jwtJsDecode from 'jwt-js-decode';
-import base64url from "base64url";
+// import * as jwtJsDecode from 'jwt-js-decode';
+// import base64url from "base64url";
 import '@simplewebauthn/server';
 
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
@@ -12,10 +12,10 @@ const db = await JSONFilePreset(__dirname + '/auth.json', {
   users: [],
 });
 
-const rpID = "localhost";
-const protocol = "http";
+// const rpID = "localhost";
+// const protocol = "http";
 const port = 5050;
-const expectedOrigin = `${protocol}://${rpID}:${port}`;
+// const expectedOrigin = `${protocol}://${rpID}:${port}`;
 
 const app = express()
 app.use(express.json())
@@ -29,18 +29,19 @@ const findUser = (email) => db.data.users.find((u) => u.email === email);
 
 // ADD HERE THE REST OF THE ENDPOINTS
 app.post('/auth/register',(req,res) => {
+  // TODO sanitize name, email
   // TODO validation: json-schema in fastify
   // validation
   //  is email empty
   //  is name empty
   //  res.status(400)
-  const user = {
+  const validatedInput = {
     name: req.body.register_name,
     email: req.body.register_email,
     password: req.body.register_password,
   }
   
-  const userFound = findUser(user.email);
+  const userFound = findUser(validatedInput.email);
 
   // TODO status code
   if (userFound) {
@@ -50,12 +51,54 @@ app.post('/auth/register',(req,res) => {
       message: 'User already exists',
     });
   } else {
-    db.data.users.push(user);
+    // TODO use async api bcrypt
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(validatedInput.password, salt);
+
+    db.data.users.push({
+      // TODO sanitize name, email before writing to database
+      name: validatedInput.name,
+      email: validatedInput.email,
+      hashedPassword,
+    });
     db.write();
 
     res.status(200);
     res.send({
       ok: true,
+    });
+  }
+});
+
+app.post('/auth/login',(req,res) => {
+  // TODO validation
+  const validatedInput = {
+    email: req.body.login_email,
+    password: req.body.login_password,
+  }
+
+  const userFound = findUser(validatedInput.email);
+
+  if (userFound) {
+    if (bcrypt.compareSync(validatedInput.password, userFound.hashedPassword)) {
+
+      res.status(200);
+      res.send({
+        ok: true,
+        name: userFound.name,
+      });
+    } else {
+      res.status(400);
+      res.send({
+        ok: false,
+        message: 'Credentials are wrong.',
+      });
+    }
+  } else {
+    res.status(400);
+    res.send({
+      ok: false,
+      message: 'Credentials are wrong.',
     });
   }
 });
