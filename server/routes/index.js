5249041bd6cd4d1b13c489d12b28983cfa46a03f
PavelPoroskov <pavel.poroskov.se@gmail.com>
2024-02-12 07:45:10 +0300

    todos

--- server/routes/index.js
+++ server/routes/index.js
@@ -19,7 +19,17 @@ export const router = express.Router();
 
 router.post('/auth/auth-options', routeAuthOptions);
 router.post('/auth/register', routeRegisterWithPassword);
+// router.post('/auth/confirm-email', );
+//  1) on registration send link in email (random string and email to table email-to-check)
+//  2) user click link to '/auth/confirm-email?ver=random-string-from-step1'
+//      find this string in table email-to-check
+//      mark email as verified
+//  ?can use magic link
 router.post('/auth/login', routeLoginWithPassword);
+// router.post('/auth/forgot-password', );
+// router.post('/auth/reset-password', );
+//  user can change password. ?temp token
+
 router.post('/auth/login-google', routeLoginGoogle);
 router.post('/auth/webauth-registration-options', routeWebAuthRegistrationOptions);
 router.post('/auth/webauth-registration-verification', routeWebAuthRegistrationVerification);
